3ebae63a1d312d45327c2a91f8cbde55e4c39e8c
Dat Quoc Nguyen <datquocnguyen@gmail.com>
2016-03-13 23:25:09 +1100

    jLDADMM version 1.0.1 commit

---
+++ src/jLDADMM.java
@@ -0,0 +1,71 @@
+import models.GibbsSamplingDMM;
+import models.GibbsSamplingLDA;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+
+import utility.CmdArgs;
+import eval.ClusteringEval;
+
+/**
+ * jLDADMM: A Java package for the LDA and DMM topic models
+ * 
+ * http://jldadmm.sourceforge.net/
+ * 
+ * @author: Dat Quoc Nguyen
+ * 
+ */
+public class jLDADMM
+{
+    public static void main(String[] args)
+    {
+        CmdArgs cmdArgs = new CmdArgs();
+        CmdLineParser parser = new CmdLineParser(cmdArgs);
+        try {
+
+            parser.parseArgument(args);
+
+            if (cmdArgs.model.equals("LDA")) {
+                GibbsSamplingLDA lda = new GibbsSamplingLDA(cmdArgs.corpus, cmdArgs.ntopics,
+                        cmdArgs.alpha, cmdArgs.beta, cmdArgs.niters, cmdArgs.twords,
+                        cmdArgs.expModelName, cmdArgs.initTopicAssgns, cmdArgs.savestep);
+                lda.inference();
+            }
+            else if (cmdArgs.model.equals("DMM")) {
+                GibbsSamplingDMM dmm = new GibbsSamplingDMM(cmdArgs.corpus, cmdArgs.ntopics,
+                        cmdArgs.alpha, cmdArgs.beta, cmdArgs.niters, cmdArgs.twords,
+                        cmdArgs.expModelName, cmdArgs.initTopicAssgns, cmdArgs.savestep);
+                dmm.inference();
+            }
+            else if (cmdArgs.model.equals("Eval")) {
+                ClusteringEval.evaluate(cmdArgs.labelFile, cmdArgs.dir, cmdArgs.prob);
+            }
+            else {
+                System.out
+                        .println("Error: Option \"-model\" must get \"LDA\" or \"DMM\" or \"Eval\"");
+                System.out.println("\tLDA: Specify the Latent Dirichlet Allocation topic model");
+                System.out
+                        .println("\tDMM: Specify the one-topic-per-document Dirichlet Multinomial Mixture model");
+                System.out.println("\tEval: Specify the document clustering evaluation");
+                help(parser);
+                return;
+            }
+        }
+        catch (CmdLineException cle) {
+            System.out.println("Error: " + cle.getMessage());
+            help(parser);
+            return;
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    public static void help(CmdLineParser parser)
+    {
+        System.out.println("java -jar jLDADMM.jar [options ...] [arguments...]");
+        parser.printUsage(System.out);
+    }
+}
